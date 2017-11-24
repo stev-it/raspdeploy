3d28e6572ec02d1e2e810c632e69a67117ce8b8a
Stev <steven.caminada@academia-schweiz.ch>
2017-11-24 14:29:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ echo "DONT FORGET TO CHANGE THE CONFIG, AS ITS EMPTY NOW!"
 sudo apt-get update && sudo apt-get install chromium zip -y
 
 #update & upgrading software which is already there
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update #&& sudo apt-get upgrade -y
 
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
 sudo apt-get remove --purge wolfram-engine libreoffice* scratch minecraft-pi sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
