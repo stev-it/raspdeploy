db698789868253821aeaffe56f2629326e2413ba
Stev <17822893+stev-it@users.noreply.github.com>
2018-03-22 11:47:00 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,6 @@ fi
 #go into tmp for safekeeping permissions
 cd /tmp
 
-#install wifi support & wifi passphrases
-sudo apt-get update && sudo apt-get install wpa_supplicant -y
-echo "DONT FORGET TO CHANGE THE CONFIG, AS ITS EMPTY NOW!"
-
-#install applications
-sudo apt-get update && sudo apt-get install chromium zip -y
-
-#update & upgrading software which is already there
-sudo apt-get update #&& sudo apt-get upgrade -y
-
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
 sudo apt-get remove --purge wolfram-engine libreoffice* scratch minecraft-pi sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
 
